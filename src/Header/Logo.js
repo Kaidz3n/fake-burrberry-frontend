e8e256890e5def351bba68918f6ffaf99122276c
Fil <vtl.filimonov@gmail.com>
2017-08-09 09:48:27 +0300

    feat: divide header to components

---
+++ src/Header/Logo.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import logo from "../assets/logo.svg";
+
+class Logo extends Component {
+  render() {
+    return <img className="logo" alt="Logo" src={logo} />;
+  }
+}
+
+export default Logo;
