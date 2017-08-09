480695ec734cd71889b894be6400a589d493b680
Fil <vtl.filimonov@gmail.com>
2017-08-09 17:17:40 +0300

    feat: add header component

---
+++ src/Header/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+import styled from "styled-components";
+
+const Content = styled.header`
+  padding: 1rem;
+  border-bottom: 1px solid #c6c6c6;
+  @media screen and (min-width: 48rem) {
+    padding: 1.5rem;
+  }
+`;
+
+const Logo = styled.img`
+  margin: auto;
+  display: block;
+
+  width: 138px;
+  height: 10px;
+`;
+
+function Header() {
+  return (
+    <Content>
+      <a href="https://ru.burberry.com/">
+        <Logo alt="Logo" src={logo} />
+      </a>
+    </Content>
+  );
+}
+
+export default Header;
